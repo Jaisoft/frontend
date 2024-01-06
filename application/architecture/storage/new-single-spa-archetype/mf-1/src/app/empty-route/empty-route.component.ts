8da08cd566a8bf1ce924ba4d1263d29e8b0cb431
Jaime Gómez Moraleda <jaime@Jaimes-MacBook-Pro-2.local>
2024-01-06 12:44:18 +0100

    pocs

--- application/architecture/storage/new-single-spa-archetype/mf-1/src/app/empty-route/empty-route.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'mf1-empty-route',
-  template: '',
-})
-export class EmptyRouteComponent {}
--- application/architecture/storage/new-single-spa-archetype/mf-2/src/app/empty-route/empty-route.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'mf2-empty-route',
-  template: '',
-})
-export class EmptyRouteComponent {}
